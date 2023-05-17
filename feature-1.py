1eb8a5181645381535e2d18031ad742317798e7c
FritzFrido <133904882+FritzFrido@users.noreply.github.com>
2023-05-17 21:47:18 +0200

    Code for feature-1 py
    here’s the py-code for the feature-1 element

---
+++ feature-1.py
@@ -0,0 +1,10 @@
+dogHunger = input("Hat der Hund hunger? Y (Ja) N (Nein)")
+
+if dogHunger == "Y":
+    print("Der Hund muss gefüttert werden!")
+elif dogHunger == "N":
+    print("Der Hund hat keinen Hunger.")
+else:
+    print("Der Hund ist beschäftigt.. ")
+
+    
